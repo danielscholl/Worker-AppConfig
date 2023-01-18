b52437699caf64b6fb61cfaca606d162d7435b28
Daniel Scholl <dascholl@microsoft.com>
2023-01-17 23:11:22 -0600

    Updated the code

--- Program.cs
+++ Program.cs
@@ -1,47 +1,73 @@
 using Azure.Identity;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Worker_AppConfig;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureAppConfiguration((context, config) =>
-    {
-      // Option 1: Get the connection string from appsettings.json (Not good because it has a key)
-      // config.AddAzureAppConfiguration(config.Build().GetConnectionString("AppConfigurationConnectionString"));
 
-
-      // Option 2: Get the connection string from an environment variable (Good because it doesn't have a key)
-      // PowerShell - $Env:ConnectionString = "connection-string-of-your-app-configuration-store"
-      // Bash -- export ConnectionString='connection-string-of-your-app-configuration-store'
-      //config.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("ConnectionString"));
-
-
-      // Option 3: Get the connection string from a user secret (Good because it doesn't have a key)
-      // dotnet user-secrets init
-      // dotnet user-secrets set "ConnectionStrings:SecretAppConfigurationConnectionString" "connection-string-of-your-app-configuration-store"
-      //config.AddAzureAppConfiguration(config.Build().GetConnectionString("SecretAppConfigurationConnectionString"));
-
-
-      // Option 4: Use Managed Identity (Good because it doesn't have a key)
-      var settings = config.Build();
-      var endpoint = settings["AppConfiguration:Endpoint"];
-      config.AddAzureAppConfiguration(options =>
-      {
-        var credential = new DefaultAzureCredential();
-        options.Connect(new Uri(endpoint), credential);
-        // options.ConfigureRefresh(refresh =>
-        // {
-        //   refresh
-        //     .Register("refreshAll", true);
-        // });
-      });
-    })
-    .ConfigureServices(configureServices)
-    .Build();
-
-host.Run();
-
-static void configureServices(HostBuilderContext context, IServiceCollection services)
+class Program
 {
-  services.Configure<WorkerConfig>(context.Configuration.GetSection("Worker:Settings"));
-  services.AddLogging();
-  services.AddHostedService<Worker>();
+  private static IConfigurationRefresher _refresher = null;
+
+  static async Task Main(string[] args)
+  {
+
+    IHost host = Host.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration((context, config) =>
+        {
+          // Option 1: Get the connection string from appsettings.json (Not good because it has a key)
+          // config.AddAzureAppConfiguration(config.Build().GetConnectionString("AppConfigurationConnectionString"));
+
+
+          // Option 2: Get the connection string from an environment variable (Good because it doesn't have a key)
+          // PowerShell - $Env:ConnectionString = "connection-string-of-your-app-configuration-store"
+          // Bash -- export ConnectionString='connection-string-of-your-app-configuration-store'
+          //config.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("ConnectionString"));
+
+
+          // Option 3: Get the connection string from a user secret (Good because it doesn't have a key)
+          // dotnet user-secrets init
+          // dotnet user-secrets set "ConnectionStrings:SecretAppConfigurationConnectionString" "connection-string-of-your-app-configuration-store"
+          //config.AddAzureAppConfiguration(config.Build().GetConnectionString("SecretAppConfigurationConnectionString"));
+
+
+          // Option 4: Use Managed Identity (Good because it doesn't have a key)
+          // Ensure that the User has the "App Configuration Data Reader" role assigned
+          var settings = config.Build();
+
+          // PowerShell - $Env:APP_CONFIG_ENDPOINT = "https://<your_endpoint>.azconfig.io"
+          // Bash -- export APP_CONFIG_ENDPOINT='https://<your_endpoint>.azconfig.io'
+          var endpoint = Environment.GetEnvironmentVariable("APP_CONFIG_ENDPOINT") ?? String.Empty;  // From environment variable
+          // var endpoint = settings["AppConfiguration:Endpoint"];  // From appsettings.json
+
+          config.AddAzureAppConfiguration(options =>
+          {
+            var credential = new DefaultAzureCredential();
+            options.Connect(new Uri(endpoint), credential);
+            options.Select("refresh", LabelFilter.Null);
+            options.Select("Worker:*", LabelFilter.Null);
+
+            options.ConfigureRefresh(refresh =>
+            {
+              refresh
+                .Register("refresh", true)
+                .SetCacheExpiration(TimeSpan.FromSeconds(5));
+            });
+
+            _refresher = options.GetRefresher();
+          });
+        })
+        .ConfigureServices(configureServices)
+        .Build();
+
+    host.Run();
+  }
+
+  static void configureServices(HostBuilderContext context, IServiceCollection services)
+  {
+    services.AddOptions<WorkerConfig>().Bind (context.Configuration.GetSection("Worker:Settings"));
+    //services.Configure<WorkerConfig>(context.Configuration.GetSection("Worker:Settings"));
+    //services.AddSingleton(context.Configuration.GetSection("Worker:Settings").Get<WorkerConfig>());
+    services.AddLogging();
+    services.AddSingleton(_refresher);
+    services.AddHostedService<Worker>();
+  }
 }
--- Worker.cs
+++ Worker.cs
@@ -1,21 +1,28 @@
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Extensions.Options;
+
 namespace Worker_AppConfig;
 
 public class Worker : BackgroundService
 {
   private readonly ILogger<Worker> _logger;
-  private readonly IConfiguration _config;
+  private readonly IOptionsMonitor<WorkerConfig> _settings;
+  private readonly IConfigurationRefresher _refresher;
 
-  public Worker(ILogger<Worker> logger, IConfiguration config)
+
+  public Worker(ILogger<Worker> logger, IOptionsMonitor<WorkerConfig> settings, IConfigurationRefresher refresher)
   {
-    _logger = logger;
-    _config = config;
+    this._logger = logger;
+    this._settings = settings;
+    this._refresher = refresher;
   }
 
   protected override async Task ExecuteAsync(CancellationToken stoppingToken)
   {
     while (!stoppingToken.IsCancellationRequested)
     {
-      _logger.LogInformation("Message: {message}", _config["Worker:Settings:Message"] ?? "Hello from Code!");
+      _logger.LogInformation("Message: {message}", _settings.CurrentValue.Message ?? "Hello from Code!"); // This one is not updating
+      await this._refresher.TryRefreshAsync();
       await Task.Delay(1000, stoppingToken);
     }
   }
